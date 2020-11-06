ada3d3711dd9c942cded54ef886c4e412588bbaa
gribovanika <gribova.ni@phystech.edu>
2020-11-06 19:09:21 +0500

    Kill Biglov

--- lab2/task1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-a = 179
-b = 197
-c = (a ** 2 + b ** 2) ** 0.5
-print (c)
--- lab2/task3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import turtle as ttl
-
-ttl.shape('turtle')
-for i in range(4):
-    ttl.forward(50)
-    ttl.left(90)
